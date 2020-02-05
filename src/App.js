21b08c492ba524acdc28ad5a1266c200b70f28e7
Patrick <pateast678@gmail.com>
2020-02-05 11:49:51 -0500

    adds base files for notes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Calculator from './Calculator';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      f
     </div>
   );
 }
